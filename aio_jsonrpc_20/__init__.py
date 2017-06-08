2fce2ebe84942e28349dc913950ca5abb9ad9ab6
Stephane LAHACHE <slahache@gmail.com>
2017-06-08 17:21:58 +0200

    fix(init.py): fix __all__ variable

--- aio_jsonrpc_20/__init__.py
+++ aio_jsonrpc_20/__init__.py
@@ -2,4 +2,9 @@ from aio_jsonrpc_20.builder import RequestBuilder, BatchRequestBuilder
 from aio_jsonrpc_20.resolver import RequestResolver
 from aio_jsonrpc_20.version import __version__
 
-__all__ = [BatchRequestBuilder, RequestBuilder, RequestResolver, __version__]
+__all__ = [
+    'BatchRequestBuilder',
+    'RequestBuilder',
+    'RequestResolver',
+    '__version__'
+]
